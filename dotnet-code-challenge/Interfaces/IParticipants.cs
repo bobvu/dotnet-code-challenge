8f05753130e6e02244bd1261df7e2c563ddb45f7
hung.vu <hung.vu@rightship.com>
2019-10-22 09:04:07 +1100

    Fix Typos

--- dotnet-code-challenge/Interfaces/IParticipants.cs
+++ dotnet-code-challenge/Interfaces/IParticipants.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using dotnet_code_challenge.Models;
 namespace dotnet_code_challenge.Interfaces
 {
     public interface IParticipants
